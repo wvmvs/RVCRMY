9f3fc4662d35973481c29701df4fa8f46149bbde
wvmvs <miguelyanezb@outlook.com>
2016-09-28 19:10:44 +0000

    inicio

---
+++ Application1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Application1.Startup))]
+namespace Application1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
